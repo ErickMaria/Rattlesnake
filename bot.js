bf5a48d5b442a6ea8294992aecf8014773665274
Erick Maria Rodrigues da Silva <erick.rodrigues.ext@conductor.com.br>
2019-01-22 13:22:52 -0300

    prototype list namespaces

--- bot.js
+++ bot.js
@@ -4,6 +4,21 @@ var SlackBot = require("slackbots");
 const shell = require('shelljs');
 require("dotenv").config();
 
+// function getNamespacesAsMensage(){
+//     var result = shell.exec("curl http://ec2-13-59-68-156.us-east-2.compute.amazonaws.com:5000/k8s/clusters/c-dpvn9/api/v1/namespaces").stdout;
+//     var RancherAPIResult = JSON.parse(result);
+//     var msg = '';
+//     RancherAPIResult.items.forEach((items, index)=>{
+//         msg = msg + (items.metadata.name + "\n");
+//     });
+
+//     return msg;
+// }
+
+// function getContainerByNamespace(namespace){
+//     var result = shell.exec(`curl http://ec2-13-59-68-156.us-east-2.compute.amazonaws.com:5000/k8s/clusters/c-dpvn9/api/v1/namespaces/${namespace}/pods`);
+// }
+
 var bot = new SlackBot({
     token: process.env.SLACK_API_TOKEN,
     name: 'rattlesnake'
@@ -17,10 +32,12 @@ bot.on('start', () => {
 
 bot.on('message', (data) => {
     // all ingoing events https://api.slack.com/rtm
-    if(data.content != undefined){
-        var messasePure = data.content.split(" ");
-        if(messasePure[messasePure.length-1] === 'hi'){
-            bot.postMessageToChannel('testes', 'Hi user, what do u wish?');
-        }
-    }
+    // if(data.content != undefined){
+    //     var messasePure = data.content.split(" ");
+    //     if(messasePure[messasePure.length-1] === 'hi'){
+    //         bot.postMessageToChannel('testes', 'Hi user, what do u wish?');
+    //     }if(messasePure[messasePure.length-1] === 'namespaces'){
+    //         bot.postMessageToChannel('testes', getNamespacesAsMensage());
+    //     }
+    // }
 });
