e259ccd3862a5cf179e214992bd18d4dc751d4c8
Erick Maria <erickmaria08@gmail.com>
2019-03-06 23:44:01 -0300

    create function for list containers

--- src/bot/interactive/bot.interactive.events.ts
+++ src/bot/interactive/bot.interactive.events.ts
@@ -3,45 +3,52 @@ import { BotServer } from "../../../core/bot/asserver/bot.server";
 import { Commands } from "../../../core/commads/bot.command";
 import { UrlBuilder } from "../../../core/builders/url/url.builder";
 import * as shellJS from 'shelljs';
+import { listContainer } from "../../../core/builders/attachments/attachments.type";
 
 export class BotInteractiveEvents extends BotEvents {
+
+    ub = new UrlBuilder();
 
     constructor(bserver: BotServer) {
         super(bserver);
     }
 
     loadEvents(): void {
-        const ub = new UrlBuilder();
 
         this.events.on('message', (event: any) => {
             var command: string = '';
 
             if (this.forceCallByName(event)) {
+                
                 event.text.trim().split(' ').forEach((element: string, index: number) => {
                     if (index > 0) {
                         command = command + element + ' ';
                     }
                 });
 
-                Commands.exec(command.trim(), () => {
-                    const data = JSON.parse(
-                        shellJS.exec(
-                            ub.builder('https', {
-                                uri: '/deployments'
-                            })
-                        ).stdout);
-
-                    var containers = '';
-
-                    data.data.forEach((element: any) => {
-                        element.containers.forEach((element: any) => {
-                            containers = containers + element.name + '\n';
-                        });
-                    });
-
-                    this.bserver.bot.chat.postMessage({ channel: event.channel, text: containers });
-                });
+                this.listContainers(command, event);
             }
+        });
+    }
+
+    listContainers(command: string, event: any){
+        Commands.exec(command, () => {
+            const data = JSON.parse(
+                shellJS.exec(
+                    this.ub.builder('https', {
+                        uri: '/deployments'
+                    })
+                ).stdout);
+
+            var containers = '';
+
+            data.data.forEach((element: any) => {
+                element.containers.forEach((element: any) => {
+                    containers = containers + element.name + '\n';
+                });
+            });
+
+            this.bserver.bot.chat.postMessage({ channel: event.channel, text: containers });
         });
     }
 
